7ec65ba5c40472a4ba8e09ea8e3b60763da463ea
Abdifattah Ahmed Hussein <142884149+Abdifatah554@users.noreply.github.com>
2023-08-23 13:28:41 +0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi🖐, I’m Abdifatah
+- 👀 I’m🎆🎉 interested in Html and css codes...
+- 🌱 I’m currently learning IT💻🖥 ...
+- 💞️ I’m looking to collaborate on ..website.
+- 📫 How to reach me Successfully...
+
+<!---
+Abdifatah554/Abdifatah554 is a ✨ special Website✨ repository because its `I'm lover codes (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
