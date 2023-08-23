6eedeb77bafdfc86585a01f2fc68b0ac7cf570f2
Abdifatah Ahmed Hussein <142884149+Abdifatah554@users.noreply.github.com>
2023-08-23 18:22:06 +0300

    Update README.me

--- README.m
+++ README.m
@@ -4,8 +4,3 @@
 - 💞️ I’m looking to collaborate on House payment app.
 💬 Aska me About**psd,css,html,c#,php**
 - 📫 How to reach me Successfully..
-
-<!---
-Abdifatah554/Abdifatah554 is a ✨ special Website✨ repository because its `I'm lover codes (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
